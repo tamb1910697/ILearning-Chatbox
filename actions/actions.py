4e03978aa2e2e147027a4ae7f69be86a14a97160
GuysKapen <guyskapen@gmail.com>
2022-07-06 15:43:32 -0800

    Add simple actions for laravel

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,17 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+import requests
+import json
+
+url = "http://localhost:8000/chatbox"
+
+
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +32,21 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+
+class ActionCheckCourses(Action):
+
+    def name(self) -> Text:
+        return "action_check_courses"
+
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        response = requests.get(f"{url}/courses")
+        message = "Here are some courses for you: "
+        if response.ok:
+            data = json.loads(response.content)
+            message += ', '.join(list(map(lambda x: x["name"], data["data"][:3])))
+
+        dispatcher.utter_message(text=message)
+
+        return []
