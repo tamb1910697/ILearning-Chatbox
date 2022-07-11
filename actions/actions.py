cc75f27d2161cc2bbed2e20042b17d0d1f90a5cc
GuysKapen <guyskapen@gmail.com>
2022-07-11 15:16:20 -0800

    Add search courses with category

--- actions/actions.py
+++ actions/actions.py
@@ -7,13 +7,11 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
+import json
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-
 import requests
-import json
+from rasa_sdk import Action, Tracker
 
 url = "http://localhost:8000/chatbox"
 
@@ -41,11 +39,31 @@ class ActionCheckCourses(Action):
     async def run(
             self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-        response = requests.get(f"{url}/courses")
-        message = "Here are some courses for you: "
+        category = None
+
+        # Search with category
+        for entity in tracker.latest_message["entities"]:
+            if entity["entity"] == "course_category":
+                category = entity["value"]
+
+        # Params for query
+        params = {}
+        if category is not None:
+            params["cats[]"] = [category]
+
+        response = requests.get(f"{url}/courses", params=params)
+        message = "Something went wrong!"
         if response.ok:
             data = json.loads(response.content)
-            message += ', '.join(list(map(lambda x: x["name"], data["data"][:3])))
+            if len(data["data"]) == 0:
+                if category is not None:
+                    message = "Sorry there is no courses for %s" % category
+                else:
+                    message = "Sorry there is no such courses"
+            else:
+                c = category + " " if category is not None else ""
+                message = f"Here are some {c}courses for you: "
+                message += ', '.join(list(map(lambda x: x["name"], data["data"][:3])))
 
         json_message = {"text": message, "link": {"url": "http://localhost:8000/courses", "title": "Show more"}}
         dispatcher.utter_message(json_message=json_message)
@@ -61,6 +79,10 @@ class ActionShowCourses(Action):
     async def run(
             self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
-        response = requests.post(f"{url}/show-courses")
+        json_message = {"text": "Here you are", "redirect": {"url": "http://localhost:8000/courses"}}
+
+        dispatcher.utter_message(json_message=json_message)
 
         return []
+
+# tracker = SQLTrackerStore(host='postgres', username='rasa', password='rasa-chatbox', db='rasa', dialect='postgresql')
